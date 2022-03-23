a94038d2e433d63e8d8bbf188db36a62570f1769
Muhammad Urwatil Wutsqo <60605671+wutsqo@users.noreply.github.com>
2022-03-23 23:50:47 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright (C) 2020-2022 Cicak Bin Kadal
 
-WEEK="04"
+WEEK="05"
 
 # This free document is distributed in the hope that it will be 
 # useful, but WITHOUT ANY WARRANTY; without even the implied 
